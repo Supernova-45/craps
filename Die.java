f07efb645db6a3132c5fbfcd692c26357697395c
Alexandra Kim <alexandrasuriya@gmail.com>
2022-01-11 14:39:01 -0800

    Finished Die.java

--- Die.java
+++ Die.java
@@ -1,3 +1,14 @@
 public class Die {
-    
+
+    //fields
+    private int roll;
+
+    //constructor
+    public Die() {
+        roll = (int) (Math.random() * 6);
+    }
+
+    public int getRoll() {
+        return roll;
+    }    
 }

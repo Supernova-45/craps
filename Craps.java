c1e9c6145610b4e14b13814b5868215a76575bea
Alexandra Kim <alexandrasuriya@gmail.com>
2022-01-12 12:09:51 -0800

    Added intro and instructions

--- Craps.java
+++ Craps.java
@@ -4,10 +4,17 @@ public class Craps {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("LET'S PLAY CRAPS!");
-        System.out.println("Do you need instructions (Y/n)?");
+        System.out.println("Do you need instructions (y/n)?");
         if (in.nextLine().equals("y")) {
-            
+            System.out.println("1. Roll two six-sided dice.");
+            System.out.println("2. a. On first roll, if you get a 7 or 11 you win!");
+            System.out.println("2. b. On first roll, if you get a 2, 3, or 12 you lose!");
+            System.out.println("2. c. Any other number you don't win or lose. The die roll becomes your 'point.'");
+            System.out.println("3. Keep rolling the dice again until:");
+            System.out.println("4. a. You roll the point again and win!");
+            System.out.println("4. b. or you roll a 7 and lose.)");
         }
-
+        System.out.println();
+        System.out.println("Good luck!");
     }
 }

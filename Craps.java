1545b247c04e7674ffea696b50cee7a172a27cad
Alexandra Kim <alexandrasuriya@gmail.com>
2022-01-12 12:06:04 -0800

    Initialized Scanner

--- Craps.java
+++ Craps.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Craps {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("LET'S PLAY CRAPS!");
+        System.out.println("Do you need instructions (Y/n)?");
+        if (in.nextLine().equals("y")) {
+            
+        }
+
     }
 }
